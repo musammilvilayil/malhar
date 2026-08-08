04e551e8e096bcc1433d3abaeb901665e9587846
musammilvilayil <140297924+musammilvilayil@users.noreply.github.com>
2026-08-08 14:37:59 +0530

    Use enhanced cinematic homepage experience

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { Toaster } from "sonner";
 import { Nav } from "@/components/site/Nav";
 import { Footer } from "@/components/site/Footer";
-import Home from "@/pages/HomeStory";
+import Home from "@/pages/HomeExperience";
 import DonatePage from "@/pages/Donate";
 import ContactPage from "@/pages/Contact";
 import {
