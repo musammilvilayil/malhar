58e8c2dcf474b75eb170b3fd85669377c332b069
musammilvilayil <140297924+musammilvilayil@users.noreply.github.com>
2026-08-08 20:59:56 +0530

    Connect verified stats after visual journal

--- frontend/src/v2/HomeV2.jsx
+++ frontend/src/v2/HomeV2.jsx
@@ -3,6 +3,7 @@ import FounderV2 from "./components/FounderV2";
 import InstitutionsV2 from "./components/InstitutionsV2";
 import CampusLifeV2 from "./components/CampusLifeV2";
 import GalleryV2 from "./components/GalleryV2";
+import StatsV2 from "./components/StatsV2";
 import "./v2.css";
 
 export default function HomeV2() {
@@ -13,6 +14,7 @@ export default function HomeV2() {
       <InstitutionsV2 />
       <CampusLifeV2 />
       <GalleryV2 />
+      <StatsV2 />
     </div>
   );
 }
