5e6bb21c9bade6696a4784a8ccd8d2894a6f60d3
musammilvilayil <140297924+musammilvilayil@users.noreply.github.com>
2026-08-08 18:02:11 +0530

    Connect Campus Life to V2 homepage

--- frontend/src/v2/HomeV2.jsx
+++ frontend/src/v2/HomeV2.jsx
@@ -2,6 +2,7 @@ import HeroV2 from "./components/HeroV2";
 import FounderV2 from "./components/FounderV2";
 import QuranVersesV2 from "./components/QuranVersesV2";
 import InstitutionsV2 from "./components/InstitutionsV2";
+import CampusLifeV2 from "./components/CampusLifeV2";
 import "./v2.css";
 
 export default function HomeV2() {
@@ -11,6 +12,7 @@ export default function HomeV2() {
       <FounderV2 />
       <QuranVersesV2 />
       <InstitutionsV2 />
+      <CampusLifeV2 />
     </div>
   );
 }
